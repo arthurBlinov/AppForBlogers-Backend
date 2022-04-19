5e6afda37b36e6b48456aef4288765711c2638a5
arthurBlinov <94515368+arthurBlinov@users.noreply.github.com>
2022-04-19 15:26:42 +0300

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary');
 
 cloudinary.config({
     cloud_name: 'mrarthur',
-    api_key: 285172745916438,
-    api_secret: '23FmsJaQQMyF6hiqVTw7IMMdHac'
+    api_key: here is my key,
+    api_secret: 'here is my api_secret'
 })
 
 const cloudinaryUploadImg = async(fileToUpload) => {
